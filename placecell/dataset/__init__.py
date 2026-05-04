79dccac421807b7cb981d4a115d0faeaae1266da
t-sasatani <sasatani.dev@gmail.com>
2026-05-05 00:25:37 +0900

    Export StabilitySplitResult and fix package docstring

--- placecell/dataset/__init__.py
+++ placecell/dataset/__init__.py
@@ -1,13 +1,19 @@
 """Dataset classes for place cell analysis."""
 
 from placecell.dataset.arena import ArenaDataset
-from placecell.dataset.base import BasePlaceCellDataset, UnitResult, unique_bundle_path
+from placecell.dataset.base import (
+    BasePlaceCellDataset,
+    StabilitySplitResult,
+    UnitResult,
+    unique_bundle_path,
+)
 from placecell.dataset.maze import MazeDataset
 
 __all__ = [
     "ArenaDataset",
     "BasePlaceCellDataset",
     "MazeDataset",
+    "StabilitySplitResult",
     "UnitResult",
     "unique_bundle_path",
 ]
